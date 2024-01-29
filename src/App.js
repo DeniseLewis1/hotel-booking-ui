f9a54b3c31521e539bf4d1dab8e6798cbe8b4e82
Denise <deniselewis1120@gmail.com>
2024-01-29 09:24:37 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Hotels from "./Hotels";
+import Filters from "./Filters";
+
+class App extends Component {
+  state = { selectedFilters: [] };
+  toggleFilter = (clickedFilterKey) => {
+    let newFilters;
+    const alreadySelected = this.state.selectedFilters.includes(
+      clickedFilterKey
+    );
+    if (alreadySelected) {
+      newFilters = this.state.selectedFilters.filter(
+        (selectedFilter) => selectedFilter !== clickedFilterKey
+      );
+    } else {
+      newFilters = this.state.selectedFilters.concat(clickedFilterKey);
+    }
+    this.setState({ selectedFilters: newFilters });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Filters
+          selectedFilters={this.state.selectedFilters}
+          toggleFilter={this.toggleFilter}
+        />
+        <Hotels selectedFilters={this.state.selectedFilters} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Filters/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Filter from "./Filter.js";
+import { filterOptions } from "../data.js";
+
+const FiltersList = (props) => {
+  const renderFilter = (filter) => {
+    const isChecked = props.selectedFilters.includes(filter.key);
+    return (
+      <Filter
+        key={filter.key}
+        filter={filter}
+        isChecked={isChecked}
+        toggleFilter={props.toggleFilter}
+      />
+    );
+  };
+  const filterListElements = filterOptions.map(renderFilter);
+  return (
+    <div className="filters">
+      <h5 className="filters__header">Filter By:</h5>
+      <hr />
+      <ul className="filters-list">{filterListElements}</ul>
+    </div>
+  );
+};
+
+export default FiltersList;
